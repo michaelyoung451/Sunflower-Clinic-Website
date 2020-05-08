1d008d22183b752782f564ca677102cee314787d
Michael <michaelyoung451@gmail.com>
2020-05-07 22:48:52 -0500

    Created Abstract Entity, Set up MySQL, and added JPA implementation.

---
+++ src/main/java/com/sunflowerclinicmassage/website/controllers/AdminController.java
@@ -0,0 +1,14 @@
+package com.sunflowerclinicmassage.website.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("admin")
+    public String index(Model model) {
+        return "/admin/index";
+    }
+}
